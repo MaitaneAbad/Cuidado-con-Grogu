e018cf946891606926366ddc0844f40eb935b137
MaitaneAbad <maitaneabadconde@gmail.com>
2022-01-26 14:28:10 +0100

    add button repeat game

--- src/components/Lost.js
+++ src/components/Lost.js
@@ -3,9 +3,17 @@ import { NavLink } from 'react-router-dom';
 const Lost = (props) => {
   return (
     <div className={`containerLost ${props.lost}`}>
-      <p className='containerLost__title'>Ohhh, ¡Grogu se ha comido todo!</p>
+      <p className='containerLost__title'>
+        Ohhh, ¡Grogu se ha comido todo!, Vuelve a intentarlo
+      </p>
       <div className='containerLost__menu'>
-        <NavLink to='/'>volver al menú principal</NavLink>
+        <NavLink to='/'>
+          <input
+            className='containerLost__menu--button'
+            type='button'
+            value='volver al menú principal'
+          />
+        </NavLink>
       </div>{' '}
     </div>
   );
--- src/components/Win.js
+++ src/components/Win.js
@@ -5,7 +5,13 @@ const Win = (props) => {
     <div className={`containerWin ${props.win}`}>
       <p className='containerWin__title'>Mando completa la misión</p>
       <div className='containerWin__menu'>
-        <NavLink to='/'>volver al menú principal</NavLink>
+        <NavLink to='/'>
+          <input
+            className='containerWin__menu--button'
+            type='button'
+            value='volver al menú principal'
+          />
+        </NavLink>
       </div>
     </div>
   );
